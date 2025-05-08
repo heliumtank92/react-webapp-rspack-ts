77350ed50443d4d6e345590b88c67816c75f6079
Ankit Gandhi <ankitgandhi452@gmail.com>
2025-05-08 13:15:58 +0530

    feat(commit-lint): new config
    added new config for maintaining format of the commit messages
    
    References: Code Commit Rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,1 +1,38 @@
-export default { extends: ['@commitlint/config-conventional'] }
+import type { UserConfig } from '@commitlint/types'
+
+const configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Header
+    'type-empty': [2, 'never'],
+    // Allowed type enums
+    // 'type-enum': [2,'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      ['env', 'redux', 'packages', 'breaking-change', 'error', 'commit-lint']
+    ], // define allowed scopes as per your project
+    'subject-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'], // lowercase summary
+    'header-full-stop': [2, 'never', '.'], // no full stop at the end of header
+
+    // Body
+    'body-empty': [2, 'never'],
+    'body-leading-blank': [2, 'always'],
+
+    // Footer
+    'footer-empty': [2, 'never'],
+    'footer-leading-blank': [2, 'always'],
+    // Trailer token must exist
+    'trailer-exists': [2, 'always', 'References']
+    // 'references-empty': [2, 'never'], // references values (Fixes, Closes, Resolves)
+  },
+  parserPreset: {
+    parserOpts: {
+      noteKeywords: ['References'] // parser preset to help Commitlint recognize custom trailers
+    }
+  }
+}
+
+export default configuration
